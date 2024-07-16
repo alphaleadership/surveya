3e780504e51677715022a8ffb9ff0de8de4412c2
alphaleadership <thomas.iniguez@free.fr>
2024-07-16 15:23:29 +0200

    'allpha'

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const yaml=require("json-to-pretty-yaml")
+const url=require("url")
 const fs=require("fs")
 const site =fs.readFileSync("./sites").toString().split("\n")
 fs.writeFileSync("./sites.yaml",yaml.stringify({sites:site.map((sit,i)=>{
-    return {url:sit.replace("\r",""),name:i}
+    console.log(sit)
+    const gamma=new url.URL(sit.replace("\r",""))
+    return {url:sit.replace("\r",""),name:gamma.hostname}
 })}))
